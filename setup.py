8926cfb4f7159065943cd52b315c0b6ec4058914
Pinaki <appugupta007@gmail.com>
2023-05-24 13:48:32 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+import re
+from os import path
+
+FILE_DIR = path.dirname(path.abspath(path.realpath(__file__)))
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    install_requirement = f.readlines()
+
+with open(path.join(FILE_DIR, 'chatgpt_wrapper', 'version.py')) as f:
+    version = re.match(r'^__version__ = "([\w\.]+)"$', f.read().strip())[1]
+
+setup(
+    name="chatGPT",
+    version=version,
+    author="Mahmoud Mabrouk",
+    author_email="mahmoudmabrouk.mail@gmail.com",
+    description="A simple Python class for interacting with OpenAI's chatGPT using Playwright",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mmabrouk/chatgpt-wrapper",
+    packages=find_packages(),
+    install_requires=install_requirement,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            "chatgpt = chatgpt_wrapper.main:main"
+        ]
+    },
+    scripts=["postinstall.sh"],
+)
